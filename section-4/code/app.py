020e07d6ca8e99c344099f53835bba60057d00b8
Carl Larsson <carl.larsson.diadrom@gmail.com>
2021-09-29 20:58:38 +0200

    finished section-4 except JWT part

--- section-4/code/app.py
+++ section-4/code/app.py
@@ -1,25 +1,53 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'calle'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
 
 class Item(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('price', 
+                        type=float, 
+                        required=True, 
+                        help="This field cannot be left blank")
+
+    #@jwt_required()
     def get(self, name):
-        for item in items:
-            if name == item['name']:
-                return item
-        return {'item': None}, 404
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        data = request.get_json()
+        if  next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': f"An item with name '{name}' already exists in database"}, 400
+        data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
+    
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
 
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 class ItemList(Resource):
     def get(self):
